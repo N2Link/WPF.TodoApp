862f4ecb4ad1775127a9f18dbaf931e15fa296d4
shjnna1294 <shjnna1294@gmail.com>
2025-02-12 22:50:36 +0700

    Add project files.

---
+++ TodoApp.UI/Commands/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace TodoApp.UI.Commands
+{
+    using System.Windows.Input;
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <inheritdoc/>
+        public bool CanExecute(object? parameter)
+        {
+            ArgumentNullException.ThrowIfNull(parameter);
+            return this.canExecute is null || this.canExecute(parameter);
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object? parameter)
+        {
+            ArgumentNullException.ThrowIfNull(parameter);
+            this.execute(parameter);
+        }
+    }
+}
---
+++ TodoApp.UI/ViewModels/TodoAppViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.UI.ViewModels
+{
+    class TodoAppViewModel
+    {
+        public ObservableCollection<string> Tasks { get; set; }
+
+        public TodoAppViewModel()
+        {
+            this.Tasks = new ObservableCollection<string>()
+            {
+                "Learn WPF",
+                "Do the excersies",
+            };
+        }
+    }
+}
